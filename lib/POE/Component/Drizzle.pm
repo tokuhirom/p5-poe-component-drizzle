4231ca6a338c3d253d39eb2f9f1014d8ed8ef2c7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-06-23 08:09:15 +0900

    initial commit

---
+++ lib/POE/Component/Drizzle.pm
@@ -0,0 +1,33 @@
+package POE::Component::Drizzle;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+POE::Component::Drizzle -
+
+=head1 SYNOPSIS
+
+  use POE::Component::Drizzle;
+
+=head1 DESCRIPTION
+
+POE::Component::Drizzle is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
